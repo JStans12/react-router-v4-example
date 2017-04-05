01ba4ea99c42113da89f762084f40338c3769314
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-04-05 13:23:59 -0600

    implement links

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
   render() {
     return(
       <div>
-        <p>header</p>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/roster'>Roster</Link></li>
+          <li><Link to='/schedule'>Schedule</Link></li>
+        </ul>
       </div>
     );
   }
