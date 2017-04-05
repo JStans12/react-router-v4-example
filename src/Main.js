92e9f0a392cbe7c55891812828c4fca23291d4da
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-04-05 13:29:06 -0600

    create not found page

--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './Home.js'
 import Roster from './Roster.js'
 import Schedule from './Schedule.js'
+import NotFound from './NotFound.js'
 
 const Main = () => (
   <main>
@@ -10,6 +11,7 @@ const Main = () => (
       <Route exact path='/' component={Home}/>
       <Route path='/roster' component={Roster}/>
       <Route path='/schedule' component={Schedule}/>
+      <Route path='*' component={NotFound}/>
     </Switch>
   </main>
 )
